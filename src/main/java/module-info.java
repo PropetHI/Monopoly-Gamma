18e22e426a553f9345a81ea5bbd91bed03647641
Adrian Roszkowski <adr11@hi.is>
2024-04-01 16:50:30 +0000

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module adrian.roszkowski.monopolygamma {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens adrian.roszkowski.monopolygamma to javafx.fxml;
+    exports adrian.roszkowski.monopolygamma;
+}
